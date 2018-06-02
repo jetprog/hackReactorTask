29e6639f7fbeff19d4280bd4a7448f0a6278d7db
Seth Jetro <jsethjetro@gmail.com>
2018-06-01 20:12:42 -0500

    user can store multiple function in the local storage, can list all function stored, seacrh and update, delete a function. using the prompt

--- app.js
+++ app.js
@@ -1,15 +1,77 @@
 $(document).ready(function(){
-	
+
+	var obj = {};
 	$('.submitForm').click(function(){
-		$value = $('.textField').val();
-		localStorage.setItem("textFieldValue", $value);
+		 var $codeName = $('.textField').val();
+		 var $snippet = $('.code').val();
+
+		if(localStorage.getItem("object") === null){
+			obj[$codeName] = $snippet;
+			var jsonObject = JSON.stringify(obj);
+			localStorage.setItem("object", jsonObject);
+		}
+		else{
+
+			var jsonString = localStorage.getItem("object");
+			var getObject = JSON.parse(jsonString);
+			getObject[$codeName] = $snippet;
+			var jsonObject = JSON.stringify(getObject);
+			localStorage.setItem("object", jsonObject);
+		}
+
+
 		$('.textField').val('');
+		$('.code').val('');
+
 	});
 
-	$('.retrieveData').click(function(){
-		var $data = localStorage.getItem("textFieldValue");
-		console.log($data);
-		var $div = "<div><h2>"+$data+"</h2></div>"
-		$('body').append($div);
+	$('.showSnippet').click(function(){
+		var data = JSON.parse(localStorage.getItem("object"));
+		if(Object.keys(data).length>0){
+			$('.listSnippet').toggle();
+			$('.list').prepend("<h4>List of snippets</h4>")
+
+			for(key in data){
+				var $test = "<li>"+key+"</li>";
+				$('.listSnippet').append($test);
+			}
+		}
+
 	});
+
+	$('.searchSnippet').click(function(){
+		var data = JSON.parse(localStorage.getItem("object"));
+		if(Object.keys(data).length>0){
+			var message = prompt("Write the name of the function you search"," ");
+			if(data.hasOwnProperty(message)){
+				$('.textField').val(message);
+				$('.code').val(data[message]);
+				$('.textField').attr('readonly', true);
+			}
+			else{
+				alert("Not exist");
+			}
+
+		}
+		
+		//localStorage.setItem("object", JSON.stringify(data))
+	});
+	
+	$('.deleteSnippet').click(function(){
+		var data = JSON.parse(localStorage.getItem("object"));
+		if(Object.keys(data).length>0){
+			var message = prompt("Write the name of your function you want to delete"," ");
+			if(data.hasOwnProperty(message)){
+				delete data[message];
+				localStorage.setItem("object", JSON.stringify(data));
+				alert('Function ' +message+ ' was deleted')
+			}
+			else{
+				alert("Not exist");
+			}
+		}
+
+	});
+
+
 });
